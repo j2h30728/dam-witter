37e4893661457389f3945744375fb8c71f93e05a
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-17 21:41:06 +0900

    feat: auth API Route 생성

---
+++ src/pages/api/users/create-account.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { METHOD } from '@/constants/method';
+import db from '@/libs/server/db';
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import bcrypt from 'bcrypt';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const { email, name, password } = req.body;
+
+  if (!email || !name) {
+    return res.status(404).json({ isSuccess: false, message: '잘못된 입력입니다.' });
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (user) {
+    return res.status(409).json({
+      isSuccess: false,
+      message: '이미 존재하는 email 입니다.',
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  return res.status(200).json({ isSuccess: true, message: '회원가입 완료되었습니다' });
+}
+
+export default withHandler({ handler, methods: [METHOD.POST] });
---
+++ src/pages/api/users/log-in.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { METHOD } from '@/constants/method';
+import db from '@/libs/server/db';
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import { withApiSession } from '@/libs/server/withSession';
+import bcrypt from 'bcrypt';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(404).json({ isSuccess: false, message: '잘못된 입력입니다.' });
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (!user) {
+    return res.status(409).json({
+      isSuccess: false,
+      message: '잘못된 입력입니다.',
+    });
+  }
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    return res.status(400).json({ isSuccess: false, message: '잘못된 비밀번호입니다.' });
+  }
+
+  req.session.user = { id: user.id };
+  await req.session.save();
+
+  return res.status(200).json({ isSuccess: true, message: '로그인 되었습니다.' });
+}
+
+export default withApiSession(withHandler({ handler, methods: [METHOD.POST] }));
