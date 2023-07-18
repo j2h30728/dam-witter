f49886b7c48efb7ded29a3f4090075449840a063
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-18 16:57:43 +0900

    feat: redirect middleware 추가

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import type { NextRequest } from 'next/server';
+
+import { NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/create-account')) {
+    if (req.cookies.has('dam-witter')) {
+      return NextResponse.redirect(new URL('/', req.url));
+    } else {
+      return NextResponse.next();
+    }
+  }
+
+  if (!req.cookies.has('dam-witter') && !req.nextUrl.pathname.startsWith('/log-in')) {
+    return NextResponse.redirect(new URL('/log-in', req.url));
+  }
+}
+export const config = {
+  matcher: ['/((?!api|_next/static|favicon.ico).*)'],
+};
