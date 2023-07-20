265086b028f86065e4a8a5936e78a275803bce42
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-20 16:11:10 +0900

    fix: withHandler 함수내 타입 수정

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -8,17 +8,17 @@ interface ConfigType {
 }
 
 export default function withHandler({ handler, isPrivate, methods }: ConfigType) {
-  return async function (req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  return async function (req: NextApiRequest, res: NextApiResponse<ResponseType<null>>) {
     if (req.method && !methods.includes(req.method as any)) {
       return res.status(405).end();
     }
     if (isPrivate && !req.session.user) {
-      return res.status(401).json({ isSuccess: false, message: 'aaaa잘못된 접근입니다.' });
+      return res.status(401).json({ data: null, isSuccess: false, message: '잘못된 접근입니다.', statusCode: 401 });
     }
     try {
       await handler(req, res);
     } catch (error) {
-      return res.status(500).json({ isSuccess: false, message: JSON.stringify(error) });
+      return res.status(500).json({ data: null, isSuccess: false, message: JSON.stringify(error), statusCode: 500 });
     }
   };
 }
