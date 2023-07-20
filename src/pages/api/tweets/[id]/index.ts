890da789f8e4afc1f4a036b99b4f6e95941e02b8
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-20 16:17:34 +0900

    feat: 디테일커밋과 좋아요 api 생성

---
+++ src/pages/api/tweets/[id]/index.ts
@@ -0,0 +1,72 @@
+import type { ResponseType, TweetResponse } from '@/types';
+
+import { METHOD } from '@/constants';
+import { db } from '@/libs/server';
+import { withApiSession, withHandler } from '@/libs/server';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<TweetResponse | TweetResponse[]>>) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  if (req.method === METHOD.GET) {
+    const tweet = await db.tweet.findUnique({
+      include: {
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+        user: true,
+      },
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!tweet)
+      return res.status(400).json({ data: null, isSuccess: false, message: '잘못된 접근입니다.', statusCode: 400 });
+
+    const isLiked = Boolean(
+      await db.like.findFirst({
+        where: {
+          tweetId: tweet.id,
+          userId: user?.id,
+        },
+      })
+    );
+
+    return res.status(200).json({ data: tweet, isLiked, isSuccess: true, message: null, statusCode: 200 });
+  }
+
+  if (req.method === METHOD.POST) {
+    const {
+      body: { text },
+      session: { user },
+    } = req;
+
+    console.log(text, user);
+    const newTweet = await db.tweet.create({
+      data: {
+        text,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+      include: {
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+        user: true,
+      },
+    });
+    return res.status(201).json({ data: newTweet, isSuccess: true, message: null, statusCode: 201 });
+  }
+}
+export default withApiSession(withHandler({ handler, methods: [METHOD.GET] }));
---
+++ src/pages/api/tweets/[id]/like.ts
@@ -0,0 +1,46 @@
+import type { ResponseType, TweetResponse } from '@/types';
+
+import { METHOD } from '@/constants';
+import { withApiSession, withHandler } from '@/libs/server';
+import { db } from '@/libs/server';
+import { Like } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Like>>) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  const alreadyExists = await db.like.findFirst({
+    where: {
+      tweetId: Number(id),
+      userId: user?.id,
+    },
+  });
+
+  if (alreadyExists) {
+    await db.like.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+    return res.status(200).json({ data: null, isSuccess: true, message: '좋아요가 삭제되었습니다.', statusCode: 200 });
+  }
+  const like = await db.like.create({
+    data: {
+      tweet: {
+        connect: {
+          id: Number(id),
+        },
+      },
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  return res.status(200).json({ data: like, isSuccess: true, message: '좋아요가 활성화되었습니다.', statusCode: 200 });
+}
+export default withApiSession(withHandler({ handler, methods: [METHOD.POST] }));
