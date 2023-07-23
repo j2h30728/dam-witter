33c585371d7869da72213f9ff0105dd8101d7758
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-24 01:20:59 +0900

    chore: 사용하지 않는 파일 삭제

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
-export { default as useControlModal } from './useControlModal';
 export { default as useForm } from './useForm';
 export { default as useInput } from './useInput';
-export { default as useLockBodyScroll } from './useLockBodyScroll';
 export { default as useSelectImage } from './useSelectImage';
