5c9487379195ebc8e48d18cc6ce03d3ba73ebd1f
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-24 20:24:33 +0900

    chore: console.log 삭제

--- src/pages/api/files.ts
+++ src/pages/api/files.ts
@@ -25,7 +25,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Im
       }
     )
   ).json();
-  console.log(response);
 
   return res.json({
     data: { ...response.result },
