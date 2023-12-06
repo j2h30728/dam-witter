3f24823aeb194e0319703c5c7d4a03081878989d
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-06 22:27:24 +0900

    fix: like API 를 POST에서 POST/DELETE로 분리, 중복을 처리하기위해 upsert 채택

--- src/pages/api/tweets/[id]/like.ts
+++ src/pages/api/tweets/[id]/like.ts
@@ -1,46 +1,77 @@
 import type { ResponseType, TweetResponse } from '@/types';
 
 import { METHOD } from '@/constants';
-import { withApiSession, withHandler } from '@/libs/server';
 import { db } from '@/libs/server';
+import { withApiSession, withHandler } from '@/libs/server';
 import { Like } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Like>>) {
   const {
+    method,
     query: { id },
     session: { user },
   } = req;
+  try {
+    if (!user) {
+      throw Error('로그인이 필요합니다.');
+    }
+    await db.$transaction(async prisma => {
+      if (method === METHOD.POST) {
+        const like = await prisma.like.upsert({
+          create: {
+            tweet: {
+              connect: {
+                id: String(id),
+              },
+            },
+            user: {
+              connect: {
+                id: user?.id,
+              },
+            },
+          },
+          update: {},
+          where: {
+            tweetId_userId: {
+              tweetId: String(id),
+              userId: user?.id,
+            },
+          },
+        });
 
-  const alreadyExists = await db.like.findFirst({
-    where: {
-      tweetId: String(id),
-      userId: user?.id,
-    },
-  });
+        return res
+          .status(200)
+          .json({ data: like, isSuccess: true, message: '좋아요가 처리되었습니다.', statusCode: 200 });
+      }
 
-  if (alreadyExists) {
-    await db.like.delete({
-      where: {
-        id: alreadyExists.id,
-      },
+      if (method === METHOD.DELETE) {
+        const alreadyExists = await db.like.findFirst({
+          where: {
+            tweetId: String(id),
+            userId: user?.id,
+          },
+        });
+        if (alreadyExists) {
+          await prisma.like.delete({
+            where: {
+              tweetId_userId: {
+                tweetId: String(id),
+                userId: user.id,
+              },
+            },
+          });
+        }
+
+        return res
+          .status(200)
+          .json({ data: null, isSuccess: true, message: '좋아요가 삭제되었습니다.', statusCode: 200 });
+      }
     });
-    return res.status(200).json({ data: null, isSuccess: true, message: '좋아요가 삭제되었습니다.', statusCode: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      res.status(400).json({ data: null, isSuccess: false, message: error.message, statusCode: 400 });
   }
-  const like = await db.like.create({
-    data: {
-      tweet: {
-        connect: {
-          id: String(id),
-        },
-      },
-      user: {
-        connect: {
-          id: user?.id,
-        },
-      },
-    },
-  });
-  return res.status(200).json({ data: like, isSuccess: true, message: '좋아요가 활성화되었습니다.', statusCode: 200 });
 }
-export default withApiSession(withHandler({ handler, methods: [METHOD.POST] }));
+
+export default withApiSession(withHandler({ handler, methods: [METHOD.POST, METHOD.DELETE] }));
