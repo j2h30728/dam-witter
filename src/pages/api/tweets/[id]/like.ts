b65d4a92c99591cb1d6823057c0092f85f52a542
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-07 08:30:45 +0900

    feat: upsert 를 create로 변경

--- src/pages/api/tweets/[id]/like.ts
+++ src/pages/api/tweets/[id]/like.ts
@@ -4,6 +4,7 @@ import { METHOD } from '@/constants';
 import { db } from '@/libs/server';
 import { withApiSession, withHandler } from '@/libs/server';
 import { Like } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Like>>) {
@@ -17,9 +18,21 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Li
       throw Error('로그인이 필요합니다.');
     }
     await db.$transaction(async prisma => {
+      const alreadyExists = await prisma.like.findUnique({
+        where: {
+          tweetId_userId: {
+            tweetId: String(id),
+            userId: user?.id,
+          },
+        },
+      });
       if (method === METHOD.POST) {
-        const like = await prisma.like.upsert({
-          create: {
+        if (alreadyExists) {
+          return res.status(204).end();
+        }
+
+        const like = await prisma.like.create({
+          data: {
             tweet: {
               connect: {
                 id: String(id),
@@ -31,27 +44,13 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Li
               },
             },
           },
-          update: {},
-          where: {
-            tweetId_userId: {
-              tweetId: String(id),
-              userId: user?.id,
-            },
-          },
         });
-
         return res
           .status(200)
-          .json({ data: like, isSuccess: true, message: '좋아요가 처리되었습니다.', statusCode: 200 });
+          .json({ data: like, isSuccess: true, message: '좋아요가 처리되었습니다.', statusCode: 201 });
       }
 
       if (method === METHOD.DELETE) {
-        const alreadyExists = await db.like.findFirst({
-          where: {
-            tweetId: String(id),
-            userId: user?.id,
-          },
-        });
         if (alreadyExists) {
           await prisma.like.delete({
             where: {
@@ -62,13 +61,23 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Li
             },
           });
         }
-
         return res
           .status(200)
           .json({ data: null, isSuccess: true, message: '좋아요가 삭제되었습니다.', statusCode: 200 });
       }
     });
   } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code === 'P2002') {
+        return res.status(409).json({
+          data: null,
+          isSuccess: false,
+          message: '이미 존재하는 데이터입니다.',
+          statusCode: 409,
+        });
+      }
+    }
+
     if (error instanceof Error)
       res.status(400).json({ data: null, isSuccess: false, message: error.message, statusCode: 400 });
   }
