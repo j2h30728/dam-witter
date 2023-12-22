f40c3fd18e59dc6520858564dc4f718257d90088
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-23 04:39:46 +0900

    feat: tweet list 페이지에  infinite scroll 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,23 @@ import { Layout, LikeButton, LoadingSpinner, ProfileImage, Symbol, TweetImage } 
 import { METHOD, ROUTE_PATH } from '@/constants';
 import useLikeTweet from '@/hooks/tweets/useLikeTweet';
 import useDebounce from '@/hooks/useDebounce';
+import useGetInfiniteData from '@/hooks/useInfiniteScrollData';
 import { formatDate, maskEmail } from '@/libs/client';
 import { db, withSsrSession } from '@/libs/server';
 import { ResponseType, TweetResponse } from '@/types';
 import { GetServerSidePropsContext, NextPage } from 'next';
 import Link from 'next/link';
-import useSWR, { SWRConfig } from 'swr';
+import { SWRConfig } from 'swr';
 
 const Home: NextPage = () => {
-  const {
-    data: responseTweets,
-    isLoading,
-    mutate: tweetsMutate,
-  } = useSWR<ResponseType<TweetResponse[]>>('/api/tweets');
+  const { bottomItemRef, data, isLoading, isValidating, mutate } =
+    useGetInfiniteData<ResponseType<TweetResponse[]>>('/api/tweets');
 
   const { trigger: toggleLike } = useLikeTweet();
 
   const debouncedToggleLike = useDebounce((tweet: TweetResponse) => {
     if (responseTweets) {
-      responseTweets.data?.forEach(responseTweet => {
+      responseTweets?.forEach(responseTweet => {
         if (responseTweet.id === tweet.id) {
           toggleLike(
             { method: responseTweet.isLiked ? METHOD.DELETE : METHOD.POST, tweetId: responseTweet.id },
@@ -34,32 +32,31 @@ const Home: NextPage = () => {
   const handleLikeToggle = (tweet: TweetResponse) => {
     debouncedToggleLike(tweet);
 
-    if (responseTweets)
-      tweetsMutate(
-        {
-          ...responseTweets,
-          data:
-            responseTweets.data?.map(t => {
-              if (t.id === tweet.id) {
-                let newLikeStatus = !t.isLiked;
-                let newLikeCount = newLikeStatus ? t._count.likes + 1 : t._count.likes - 1;
-                return { ...t, _count: { ...t._count, likes: newLikeCount }, isLiked: newLikeStatus };
-              } else {
-                return t;
-              }
-            }) || [],
-        },
-        false
-      );
+    // TODO infinite scroll 기준으로 좋아요 낙관적 업데이트 적용
+    if (responseTweets) mutate();
+    // {
+    //   ...responseTweets,
+    //   data:
+    //     responseTweets.data?.map(t => {
+    //       if (t.id === tweet.id) {
+    //         let newLikeStatus = !t.isLiked;
+    //         let newLikeCount = newLikeStatus ? t._count.likes + 1 : t._count.likes - 1;
+    //         return { ...t, _count: { ...t._count, likes: newLikeCount }, isLiked: newLikeStatus };
+    //       } else {
+    //         return t;
+    //       }
+    //     }) || [],
+    // },
+    // false
   };
 
   if (isLoading) {
     return <LoadingSpinner text={'불러오는 중..'} />;
   }
-
+  const responseTweets = data.flatMap(tweet => tweet.data) as TweetResponse[];
   return (
     <div className="gap-5 sub-layout">
-      {responseTweets?.data?.map((tweet: TweetResponse) => (
+      {responseTweets.map((tweet: TweetResponse) => (
         <div className="flex flex-col gap-4 pb-2 border-b-2 border-base1" key={tweet.id}>
           <div className="flex items-center gap-3 px-3">
             <ProfileImage avatarId={tweet.user.profile?.avatar} />
@@ -89,6 +86,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       ))}
+      {isValidating ? <LoadingSpinner text={'불러오는 중..'} /> : <div ref={bottomItemRef}>마지막</div>}
     </div>
   );
 };
