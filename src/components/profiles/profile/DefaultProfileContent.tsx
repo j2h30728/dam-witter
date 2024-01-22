a785d5515f20824eee263a6a367543217a20f2b7
Dam <60846068+j2h30728@users.noreply.github.com>
2024-01-23 01:42:51 +0900

    chore: 프로필 페이지에 라우팅할수  있는 곳을 색상으로 표시

--- src/components/profiles/profile/DefaultProfileContent.tsx
+++ src/components/profiles/profile/DefaultProfileContent.tsx
@@ -6,6 +6,11 @@ import useProfileContext from './useProfileContext';
 
 const DefaultProfileContent = ({ children }: { children?: React.ReactNode }) => {
   const { profile } = useProfileContext();
+  const styles = {
+    count: 'w-full p-2 text-center rounded-full hover:text-yellow3 text-brown2 border-beige3 hover:bg-white',
+    link: 'flex flex-col items-center gap-3 rounded-sm aspect-square',
+  };
+
   return (
     <div className="flex w-full">
       <div className="flex flex-col items-center w-full gap-1 px-2">
@@ -15,17 +20,17 @@ const DefaultProfileContent = ({ children }: { children?: React.ReactNode }) => 
       </div>
       <div className="flex flex-col items-start self-start justify-center w-full gap-10 mt-14">
         <div className="flex justify-center w-full gap-10 text-lg ">
-          <Link className="flex flex-col items-center gap-3" href={ROUTE_PATH.HOME}>
+          <Link className={styles.link} href={ROUTE_PATH.HOME}>
             <p>게시물</p>
-            <p>{profile.tweets.length}</p>
+            <p className={styles.count}>{profile.tweets.length}</p>
           </Link>
-          <Link className="flex flex-col items-center gap-3" href={ROUTE_PATH.FOLLOWERS(profile.profile?.userId || '')}>
+          <Link className={styles.link} href={ROUTE_PATH.FOLLOWERS(profile.profile?.userId || '')}>
             <p>팔로워</p>
-            <p>{profile.followers.length}</p>
+            <p className={styles.count}>{profile.followers.length}</p>
           </Link>
-          <Link className="flex flex-col items-center gap-3" href={ROUTE_PATH.FOLLOWING(profile.profile?.userId || '')}>
+          <Link className={styles.link} href={ROUTE_PATH.FOLLOWING(profile.profile?.userId || '')}>
             <p>팔로잉</p>
-            <p>{profile.following.length}</p>
+            <p className={styles.count}>{profile.following.length}</p>
           </Link>
         </div>
         {children}
