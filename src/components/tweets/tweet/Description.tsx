b97df26d3769beeef5df6552d541d0bb2081f237
Dam <60846068+j2h30728@users.noreply.github.com>
2024-01-23 19:59:54 +0900

    feat: 트윗 컴포넌트 내에 좋아요를 누른 사용자 리스트를 보여줄 모달 컴포넌트 생성

--- src/components/tweets/tweet/Description.tsx
+++ src/components/tweets/tweet/Description.tsx
@@ -1,23 +1,35 @@
 import LikeButton from '@/components/common/LikeButton';
 import { formatDate } from '@/libs/client';
 import { TweetResponse } from '@/types';
+import { useState } from 'react';
 
+import LikedUsersModal from './LikedUsersModal';
 import useTweetContext from './useTweetContext';
 
 export const Description = ({ onToggleLike }: { onToggleLike: (selectedTweet?: TweetResponse) => void }) => {
   const { tweet } = useTweetContext();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="flex items-center justify-between w-full gap-2">
-      <div className="flex items-center gap-3 w-fit">
-        <LikeButton isLiked={tweet?.isLiked} toggleLike={onToggleLike} />
-        <span>
-          좋아요 <strong>{tweet?._count.likes}</strong> 개
-        </span>
-        <span>
-          코멘트 <strong>{tweet?._count.comments}</strong> 개
-        </span>
+    <>
+      <div className="flex items-center justify-between w-full gap-2">
+        <div className="flex items-center gap-3 w-fit">
+          <LikeButton isLiked={tweet?.isLiked} toggleLike={onToggleLike} />
+          <div className="cursor-pointer" onClick={() => setIsModalOpen(true)}>
+            좋아요 <strong>{tweet?._count.likes}</strong> 개
+          </div>
+          <div>
+            코멘트 <strong>{tweet?._count.comments}</strong> 개
+          </div>
+        </div>
+        <small className="ml-auto text-stone-500">{formatDate(tweet?.createdAt)}</small>
       </div>
-      <small className="ml-auto text-stone-500">{formatDate(tweet?.createdAt)}</small>
-    </div>
+      <LikedUsersModal
+        isOpen={isModalOpen}
+        likedUser={tweet.likes?.map(user => user.user) || []}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
   );
 };
