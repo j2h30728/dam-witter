6bbeafe53392229153048683115edadbd84fd02e
Dam <60846068+j2h30728@users.noreply.github.com>
2024-01-26 23:12:28 +0900

    refactor: tweets api 중 전체 리스트 조회api 보완

--- src/pages/api/tweets/index.ts
+++ src/pages/api/tweets/index.ts
@@ -5,101 +5,14 @@ import { db } from '@/libs/server';
 import { withApiSession, withHandler } from '@/libs/server';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<TweetResponse | TweetResponse[]>>) {
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<TweetResponse[]>>) {
   const { user } = req.session;
   const { limit, pageIndex } = req.query;
 
   const take = Number(limit ?? 10);
   const skip = Number(pageIndex ?? 0) * take;
-
-  const tweets = await db.tweet.findMany({
-    include: {
-      _count: {
-        select: {
-          comments: true,
-          likes: true,
-        },
-      },
-      likes: {
-        include: {
-          user: {
-            select: {
-              email: true,
-              id: true,
-              name: true,
-              profile: true,
-            },
-          },
-        },
-      },
-      user: {
-        select: {
-          email: true,
-          followers: true,
-          id: true,
-          name: true,
-          profile: true,
-        },
-      },
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-    skip,
-    take,
-  });
-  const transformedTweets = tweets.map(tweet => ({
-    ...tweet,
-    isFollowing: tweet.user.followers.some(follower => follower.followerId === user?.id),
-    isLiked: tweet.likes.some(like => like.userId === user?.id),
-  }));
   if (req.method === METHOD.GET) {
-    return res.status(200).json({ data: transformedTweets, isSuccess: true, message: null, statusCode: 200 });
-  }
-
-  if (req.method === METHOD.POST) {
-    const { imageId, text } = JSON.parse(req.body);
-    const { user } = req.session;
-    if (imageId) {
-      const newTweetWithImage = await db.tweet.create({
-        data: {
-          image: imageId,
-          text,
-          user: {
-            connect: {
-              id: user?.id,
-            },
-          },
-        },
-        include: {
-          _count: {
-            select: {
-              comments: true,
-              likes: true,
-            },
-          },
-          user: {
-            select: {
-              email: true,
-              id: true,
-              name: true,
-              profile: true,
-            },
-          },
-        },
-      });
-
-      return res.status(201).json({ data: newTweetWithImage, isSuccess: true, message: null, statusCode: 201 });
-    }
-    const newTweet = await db.tweet.create({
-      data: {
-        text,
-        user: {
-          connect: {
-            id: user?.id,
-          },
-        },
-      },
+    const tweets = await db.tweet.findMany({
       include: {
         _count: {
           select: {
@@ -107,19 +20,58 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType<Tw
             likes: true,
           },
         },
+        likes: {
+          select: {
+            user: {
+              select: {
+                email: true,
+                id: true,
+                name: true,
+                profile: true,
+              },
+            },
+          },
+        },
         user: {
           select: {
             email: true,
+            followers: true,
             id: true,
             name: true,
             profile: true,
           },
         },
       },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      skip,
+      take,
     });
-    return res
-      .status(201)
-      .json({ data: [newTweet, ...transformedTweets], isSuccess: true, message: '추가 되었습니다.', statusCode: 201 });
+    const transformedTweets = tweets.map(tweet => ({
+      ...tweet,
+      isFollowing: tweet.user.followers.some(follower => follower.followerId === user?.id),
+      isLiked: tweet.likes.some(like => like.user.id === user?.id),
+    }));
+    return res.status(200).json({ data: transformedTweets, isSuccess: true, message: null, statusCode: 200 });
+  }
+
+  if (req.method === METHOD.POST) {
+    const { imageId, text } = JSON.parse(req.body);
+    const { user } = req.session;
+    await db.tweet.create({
+      data: {
+        image: imageId ?? null,
+        text,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+
+    return res.status(201).json({ data: null, isSuccess: true, message: null, statusCode: 201 });
   }
 }
 export default withApiSession(withHandler({ handler, isPrivate: true, methods: [METHOD.GET, METHOD.POST] }));
