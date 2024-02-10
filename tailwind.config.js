33aa2eab5a2fc474b8ee1719f1330fd8601912d6
Dam <60846068+j2h30728@users.noreply.github.com>
2024-02-10 22:36:08 +0900

    feat: tailwind config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
+      borderRadius: {
+        '4xl': '2rem',
+      },
       colors: {
+        active: '#9A3412',
         base: '#F5F4F5',
         base1: '#E7E5E4',
         base2: '#A1A1AA',
@@ -23,56 +27,31 @@ module.exports = {
         beige0: '#fff9f2',
         beige1: '#ffe0a6',
         beige2: '#f7dba6',
-
         beige3: '#f7c972',
         blue1: '#1fb6ff',
         blue2: '#3ABFF8',
+
         brown1: '#917b50',
         brown2: '#6e4d10',
-
         brown3: '#63563c',
         brown4: '#403a2e',
+        default: '#A8A29E',
+
         gray: '#8492a6',
-        'gray-dark': '#273444',
-        'gray-light': '#d3dce6',
-        green1: '#13ce66',
-        green2: '#36D399',
-        min5: '#1b3b3d',
-        mint1: '#b2f5f7',
-        mint2: '#65C3C8',
-        mint3: '#3FACB1',
 
-        mint4: '#19797d',
-        pink1: '#EF9FBC',
-        pink2: '#E45B8C',
-        pointContent: '#3B2600',
-        purple: '#7e5bef',
-        red1: '#F87272',
+        symbol1: '#F5B989',
+        symbol2: '#633B2E',
 
-        red2: '#50001D',
         text1: '#172337',
-
         text2: '#291334',
         text3: '#291334',
-        yellow1: '#ffc82c',
-        yellow2: '#FBBD23',
         yellow3: '#DA9413',
       },
       container: {
         center: true,
       },
-      extend: {
-        borderRadius: {
-          '4xl': '2rem',
-        },
-        spacing: {
-          '8xl': '96rem',
-          '9xl': '128rem',
-        },
-      },
       fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
+        'hanalei-fill': ['var(--font-hanalei-fill)', 'sans-serif'],
       },
       fontSize: {
         '2xl': '1.5rem',
@@ -81,8 +60,13 @@ module.exports = {
         '5xl': '3.052rem',
         base: '1rem',
         lg: '1.2rem',
+        md: '1rem',
         sm: '0.8rem',
         xl: '1.25rem',
+      },
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
       },
     },
   },
