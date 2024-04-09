bf51ceffc4b7bc47873628215c12b755b9381df1
Dam <60846068+j2h30728@users.noreply.github.com>
2024-04-09 13:59:08 +0900

    refactor: 이미지 등록 및 api 호출 최적화를 위하여 abort signal 적용 [#73]

--- src/hooks/common/useSelectImage.ts
+++ src/hooks/common/useSelectImage.ts
@@ -1,20 +1,26 @@
 import getImageId from '@/libs/client/getImageId';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function useSelectImage() {
   const [isImageLoading, setIsImageLoading] = useState(false);
   const [imageId, setImageId] = useState('');
   const [previewImage, setPreviewImage] = useState('');
+  const uploadControllerRef = useRef<AbortController | null>(null);
 
   const selectedImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (uploadControllerRef.current) {
+      uploadControllerRef.current.abort();
+    }
     if (event.target.files && event.target.files.length > 0) {
+      uploadControllerRef.current = new AbortController();
+
       setIsImageLoading(true);
 
       const file = event.target.files[0];
       const previewImageUrl = URL.createObjectURL(file);
       setPreviewImage(previewImageUrl);
 
-      const id = await getImageId(file, file.name);
+      const id = await getImageId(file, file.name, uploadControllerRef.current);
       setImageId(id);
 
       setIsImageLoading(false);
@@ -26,6 +32,10 @@ export default function useSelectImage() {
     URL.revokeObjectURL(previewImage);
     setImageId('');
     setPreviewImage('');
+    if (uploadControllerRef.current) {
+      uploadControllerRef.current.abort();
+      uploadControllerRef.current = null;
+    }
   };
 
   return { cancelImage, imageId, isImageLoading, previewImage, selectedImage };
--- src/libs/client/getImageId.ts
+++ src/libs/client/getImageId.ts
@@ -1,12 +1,41 @@
-export default async function getImageId(imageFile: File, fileName: string) {
-  const {
-    data: { uploadURL },
-  } = await (await fetch('/api/files')).json();
-  const form = new FormData();
-  form.append('file', imageFile, fileName);
-  const {
-    result: { id },
-  } = await (await fetch(uploadURL, { body: form, method: 'POST' })).json();
+import { METHOD } from '@/constants';
+import { END_POINTS } from '@/constants/api';
 
-  return id;
+export default async function getImageId(imageFile: File, fileName: string, controller: AbortController) {
+  const signal = controller.signal;
+
+  try {
+    const uploadUrlResponse = await fetch(END_POINTS.FILES, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: METHOD.GET,
+      signal,
+    });
+    if (!uploadUrlResponse.ok) throw new Error('이미지 등록에 실패하였습니다.');
+    const {
+      data: { uploadURL },
+    } = await uploadUrlResponse.json();
+
+    const form = new FormData();
+    form.append('file', imageFile, fileName);
+
+    const uploadResponse = await fetch(uploadURL, {
+      body: form,
+      method: METHOD.POST,
+      signal,
+    });
+    if (!uploadResponse.ok) throw new Error('이미지 등록에 실패하였습니다.');
+    const {
+      result: { id },
+    } = await uploadResponse.json();
+
+    return id;
+  } catch (error) {
+    if (error instanceof Error && error.name === 'AbortError') {
+      console.log('Abort Signal');
+    } else {
+      console.error('잠시후 요청 부탁드립니다.:', error);
+    }
+  }
 }
